bea3514f39cbff702f7a07eb3d0f96ca2fc0e978
Matt Conrad <mmconrad@gmail.com>
2015-03-29 16:58:26 -0700

    Enhancement: Adds images to poster; adds slot machine overlay

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,18 +9,31 @@ window.SlotMachine = SlotMachine;
 
 SlotMachine.setup_posters = function(reel, reelNum) {
   var posterAngle = 360 / SlotMachine.postersPerReel;
+
   for (var i = 0; i < SlotMachine.postersPerReel; i ++) {
+
     var poster = document.createElement('div');
     poster.className = 'poster';
     // poster.setAttribute('id', 'poster' + reelNum);
     poster.id = 'poster' + reelNum;
-
     // compute and assign the transform for this poster
     var transform = 'rotateX(' + (posterAngle * i) + 'deg) translateZ(' + SlotMachine.reelRadius + 'px)';
     poster.style.webkitTransform = transform;
+    
     // setup the image to show inside the poster
-    var content = poster.appendChild(document.createElement('p'));
-    content.textContent = i;
+    var content = poster.appendChild(document.createElement('img'));
+    // content.textContent = i; 
+    // create variable to track which group of images are on the posters
+    var imageSet = i % 3;
+    content.className = 'image';
+    content.imageSet = imageSet;
+    content.src = SlotMachine.pickPoster(reelNum, imageSet);
+    
+    // $(content).attr('src', function() {
+    //   console.log(SlotMachine.pickPoster(reelNum, imageSet))
+    //   SlotMachine.pickPoster(reelNum, imageSet);
+    // });
+
     // add the poster to the reel
     reel.appendChild(poster);
   }
@@ -34,6 +47,7 @@ SlotMachine.init = function() {
 
 // call init once the document is fully loaded
 window.addEventListener('load', SlotMachine.init, false);
+
 // start & stop spinning with play button
 $('#play').on('click', function(){
   if ( !($('#reel-1').hasClass('active')) ) {
@@ -50,28 +64,26 @@ $('#play').on('click', function(){
 
 
 SlotMachine.images = {
-  reel1: [
+  1: [
     './assets/reel1/coffee-maker2.png',
-    './assets/reel1/teapot-persian.png',
+    './assets/reel1/teapot.png',
     './assets/reel1/espresso-machine.png',
   ],
-  reel2: [
+  2: [
     './assets/reel2/coffee-filter-papers.png',
     './assets/reel2/tea-infuser-2.png',
     './assets/reel2/espresso-tamper.png'
   ],
-  reel3: [
-    './assets/reel3/coffee-grounds.jpg',
+  3: [
+    './assets/reel3/coffee-grounds.png',
     './assets/reel3/loose-tea.png',
     './assets/reel3/espresso-ground-coffee-8-oz.png'
   ]
 };
 
-SlotMachine.boot = function(container) {
-  container = $(container);   // wrap in jQuery
-  // router won't be used for much here, but now it can instantiate views on the container
-  var router = new SlotMachine.Router({el: container}); 
-  // causes Backbone to read routes
-  Backbone.history.start();
-};
+SlotMachine.pickPoster = function(reelNum, imageSet) {
+  // console.log('reelNum', reelNum, 'imageSet', imageSet, 'image string', SlotMachine.images[reelNum][imageSet]);
+  return SlotMachine.images[reelNum][imageSet];
+}
+
 // })();
